f732cf916508b99b09cbb1eb12061abf8a28f2c5
Brad Biehl <bradley.biehl@gmail.com>
2025-09-18 08:26:58 -0500

    [data-connect-setup] feat: Add TanStack Query integration to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,9 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { environment } from './environments/environment';
+import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental'
+
+const queryClient = new QueryClient();
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,5 +21,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAnalytics(() => getAnalytics()),
+    provideTanStackQuery(queryClient)
   ],
 };
