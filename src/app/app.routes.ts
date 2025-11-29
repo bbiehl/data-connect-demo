dc5fac46ba884c0ec5cd4ee81a79e5cba48438f8
Brad Biehl <bradley.biehl@gmail.com>
2025-11-29 09:33:58 -0600

    [home-feature] feat: implement home page component with routing and basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./home/components/home-page/home-page').then(m => m.HomePage),
+        title: 'Home'
+    }
+];
