b888ddc19e3c85894a5cee9196f3e3e79b6a5e95
Brad Biehl <bradley.biehl@gmail.com>
2025-12-01 07:37:32 -0600

    [footer-setup] feat: implement footer component with Angular Material toolbar and styling

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Footer {
-
+  readonly title = signal('Data Connect Demo');
+  readonly year = signal(new Date().getFullYear());
 }
