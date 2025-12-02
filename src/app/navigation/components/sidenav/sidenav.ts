54ae8106141462aa6abbfb25d78cc6b7c53056e5
Brad Biehl <bradley.biehl@gmail.com>
2025-12-02 07:21:57 -0600

    [navigation-feature] feat: implement sidenav component with Angular Material and responsive design

--- src/app/navigation/components/sidenav/sidenav.ts
+++ src/app/navigation/components/sidenav/sidenav.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
+import { Footer } from '../../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [],
+  imports: [MatSidenavModule, RouterModule, Footer],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Sidenav {
-
-}
+export class Sidenav {}
