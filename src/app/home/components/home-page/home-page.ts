63fb19d6780f5c82ea0e3557dcffc01abe90b6da
Brad Biehl <bradley.biehl@gmail.com>
2025-12-15 17:09:29 -0600

    [auth-feature] refactor: remove user display from home page and clean up imports in HomePage component

--- src/app/home/components/home-page/home-page.ts
+++ src/app/home/components/home-page/home-page.ts
@@ -1,29 +1,18 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { AuthStore } from '../../../auth/auth.store';
-import { JsonPipe } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home-page',
-  imports: [
-    JsonPipe,
-    MatCardModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-  ],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
-  authStore = inject(AuthStore);
-
   topRatedMovies = [
     { title: 'Movie Title 1', rating: 9.5, poster: 'https://picsum.photos/200/300?random=1' },
     { title: 'Movie Title 2', rating: 9.2, poster: 'https://picsum.photos/200/300?random=2' },
@@ -31,8 +20,20 @@ export class HomePage {
   ];
 
   recentlyAddedMovies = [
-    { title: 'Movie Title 4', added: '2025-11-28', poster: 'https://picsum.photos/200/300?random=4' },
-    { title: 'Movie Title 5', added: '2025-11-27', poster: 'https://picsum.photos/200/300?random=5' },
-    { title: 'Movie Title 6', added: '2025-11-26', poster: 'https://picsum.photos/200/300?random=6' },
+    {
+      title: 'Movie Title 4',
+      added: '2025-11-28',
+      poster: 'https://picsum.photos/200/300?random=4',
+    },
+    {
+      title: 'Movie Title 5',
+      added: '2025-11-27',
+      poster: 'https://picsum.photos/200/300?random=5',
+    },
+    {
+      title: 'Movie Title 6',
+      added: '2025-11-26',
+      poster: 'https://picsum.photos/200/300?random=6',
+    },
   ];
 }
